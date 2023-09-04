e648c4248a14544c41dcb118008085e48faf0779
Danny Barbosa <132908376+DannyBarbosaBR@users.noreply.github.com>
2023-09-04 08:16:38 -0300

    Add files via upload

---
+++ db/conexao.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVIDOR = "ftp.marcosdemelo.com";
+    const USUARIO = "marcosde_rootchat";
+    const SENHA = "#MM#adm00";
+    const BANCO = "marcosde_chat";
+
+    $conexao = mysqli_connect(SERVIDOR,USUARIO,SENHA,BANCO);
+?>
